0e600a1e3b984c808426d4315d0a6b9d9d209a88
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-08-21 22:45:17 +0100

    added contact form

---
+++ components/contact/contact-form.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './contact-form.module.css'
+
+const ContactForm = () => {
+  return (
+    <div>ContactForm</div>
+  )
+}
+
+export default ContactForm
