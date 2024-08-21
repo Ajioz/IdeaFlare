b9bd01f617810777aafa9573a57fdb17c0589182
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-08-21 06:54:49 +0100

    added gray-matter for parsing mardown

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,8 @@
 import React from "react";
 import Hero from "../components/HomePage/hero";
 import FeaturedPost from "../components/HomePage/featured";
+import { getFeaturedPost } from "../lib/post-util";
 
-const DUMMY_POST = [
-  {
-    title: "ideal-flare-01",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Nextjs is the react framework for production, which makes building good app sweet with perfect SEO",
-    date: "2024-08-20",
-    slug: "getting-started-nextjs",
-  },
-  {
-    title: "ideal-flare-02",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Nextjs is the react framework for production, which makes building good app sweet with perfect SEO",
-    date: "2024-08-20",
-    slug: "getting-started-nextjs2",
-  },
-  {
-    title: "ideal-flare-03",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Nextjs is the react framework for production, which makes building good app sweet with perfect SEO",
-    date: "2024-08-20",
-    slug: "getting-started-nextjs3",
-  },
-  {
-    title: "ideal-flare-04",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Nextjs is the react framework for production, which makes building good app sweet with perfect SEO",
-    date: "2024-08-20",
-    slug: "getting-started-nextjs4",
-  },
-];
 
 const HomePage = () => {
   return (
@@ -47,3 +14,7 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+export function getStaticProps() {
+  const featuredPost = getFeaturedPost()
+}
