be87b5346b2aed7c5095f9a91b418484009ec6c0
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-08-21 16:38:19 +0100

    improved image displayed

--- components/Posts/post-details/post-content.jsx
+++ components/Posts/post-details/post-content.jsx
@@ -2,13 +2,49 @@ import React from "react";
 import Markdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
+import Image from "next/image";
 
-const PostContent = ({ post }) => {
+const PostContent = (props) => {
+  const { post } = props;
+
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} author={post.author} />
-      <Markdown>{post.content}</Markdown>
+      <Markdown components={customRenderers}>{post.content}</Markdown>
     </article>
   );
 };
