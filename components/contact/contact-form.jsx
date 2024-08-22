a34f310a07823c066c08110e66bb900a702fb78b
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-08-22 13:38:57 +0100

    improved contact form

--- components/contact/contact-form.jsx
+++ components/contact/contact-form.jsx
@@ -8,66 +8,114 @@ const ContactForm = () => {
     message: "",
   });
 
-  const handleChange = (props) = (e) => {
-    setMsgDetail((prev) => ({ ...prev, [props]: e.target.value }));
+  const handleChange = (prop) => (e) => {
+    setMsgDetail((prev) => ({ ...prev, [prop]: e.target.value }));
   };
 
   const sendMessage = async (e) => {
     e.preventDefault();
-    console.log(msgDetail);
     const data = await fetch("/api/contact", {
       method: "POST",
-      body: msgDetail,
+      body: JSON.stringify(msgDetail), // Convert msgDetail to JSON string
       headers: {
         "Content-Type": "application/json",
       },
     });
 
-    const response = data.json();
-    console.log(response);
+    const { status } = await data.json();
+    if (status) {
+      setMsgDetail({
+        name: "",
+        email: "",
+        message: "",
+      });
+    }
   };
 
   return (
-    <section className={classes.contact}>
-      <h1>How Can I help you?</h1>
-      <form className={classes.form} onSubmit={sendMessage}>
-        <div className={classes.controls}>
-          <div className={classes.control}>
-            <label htmlFor="email">Your Email</label>
-            <input
-              type="email"
-              id="email"
-              value={msgDetail.email}
-              onChange={handleChange("email")}
-              required
-            />
-          </div>
-          <div className={classes.control}>
+    <section className={classes.body}>
+      <div className={classes.formContainer}>
+        <form className={classes.form} onSubmit={sendMessage}>
+          <h2 className={classes.formTitle}>I am open to work with you!</h2>
+          <div className={classes.formGroup}>
             <label htmlFor="name">Your Name</label>
             <input
               type="text"
               id="name"
               value={msgDetail.name}
               onChange={handleChange("name")}
+              placeholder="John help"
               required
             />
           </div>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="message">Your Message</label>
-          <textarea
-            rows="5"
-            id="message"
-            onChange={handleChange("message")}
-            value={msgDetail.message}
-            required
-          />
-        </div>
-        <div className={classes.actions}>
-          <button>Send Message</button>
-        </div>
-      </form>
+          <div className={classes.formGroup}>
+            <label htmlFor="email">Your Email</label>
+            <input
+              type="email"
+              id="email"
+              value={msgDetail.email}
+              onChange={handleChange("email")}
+              placeholder="john@domaim.com"
+              required
+            />
+          </div>
+          <div className={classes.formGroup}>
+            <label htmlFor="message">Your Message</label>
+            <textarea
+              rows="5"
+              id="message"
+              onChange={handleChange("message")}
+              value={msgDetail.message}
+              placeholder="Write your request here"
+              required
+            />
+          </div>
+          <button type="submit" className={classes.submitButton}>
+            Submit
+          </button>
+        </form>
+      </div>
     </section>
+    // <section className={classes.contact}>
+    //   <h1>How Can I help you?</h1>
+    //   <form className={classes.form} onSubmit={sendMessage}>
+    //     <div className={classes.controls}>
+    //       <div className={classes.control}>
+    //         <label htmlFor="email">Your Email</label>
+    //         <input
+    //           type="email"
+    //           id="email"
+    //           value={msgDetail.email}
+    //           onChange={handleChange("email")}
+    //           required
+    //         />
+    //       </div>
+    //       <div className={classes.control}>
+    //         <label htmlFor="name">Your Name</label>
+    //         <input
+    //           type="text"
+    //           id="name"
+    //           value={msgDetail.name}
+    //           onChange={handleChange("name")}
+    //           required
+    //         />
+    //       </div>
+    //     </div>
+    //     <div className={classes.control}>
+    //       <label htmlFor="message">Your Message</label>
+    //       <textarea
+    //         rows="5"
+    //         id="message"
+    //         onChange={handleChange("message")}
+    //         value={msgDetail.message}
+    //         required
+    //       />
+    //     </div>
+    //     <div className={classes.actions}>
+    //       <button>Send Message</button>
+    //     </div>
+    //   </form>
+    // </section>
   );
 };
 
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,29 +1,72 @@
+import { MongoClient } from "mongodb";
+
+export const connectDB = async () => {
+  return await MongoClient.connect(process.env.MONGODB_URL);
+};
+
+export const insertDoc = async (client, collection, doc) => {
+  const db = client.db();
+  return await db.collection(collection).insertOne(doc);
+};
+
+export const getAllDocs = async (client, collection, sort, filter) => {
+  const db = client.db();
+  return await db.collection(collection).find(filter).sort(sort).toArray();
+};
+
 const isValidEmail = (email) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 };
 
-const contactHandler = (req, res) => {
+const contactHandler = async (req, res) => {
+  let client;
+
   if (req.method === "POST") {
+    try {
+      client = await connectDB();
+    } catch (error) {
+      console.error("Failed to connect to the database:", error);
+      return res.status(500).json({ message: "Database server error" });
+    }
+
     try {
       const { email, name, message } = req.body;
 
       if (!isValidEmail(email)) {
-        return res.status(422).json({ message: "Invalid email" });
+        return res
+          .status(422)
+          .json({ status: false, message: "Invalid email" });
       }
 
       if (!name || name.trim() === "" || !message || message.trim() === "") {
-        return res.status(422).json({ message: "Invalid input" });
+        return res
+          .status(422)
+          .json({ status: false, message: "Invalid input" });
       }
 
       const newMessage = { email, name, message };
+
+      const result = await insertDoc(client, "messages", newMessage);
+      newMessage.id = result._id;
+
       return res.status(201).json({
-        message: "Messaged saved successfully!",
+        message: "Message saved successfully!",
         newMessage,
+        status: true,
       });
     } catch (error) {
-      return res.status(400).json({ message: "Error processing request" });
+      console.error("Error processing request:", error);
+      return res
+        .status(500)
+        .json({ message: "Failed to send message", status: false });
+    } finally {
+      client.close(); // Ensure the client is closed after the operations
     }
+  } else {
+    return res
+      .status(405)
+      .json({ message: "Method not allowed", status: false });
   }
 };
 
