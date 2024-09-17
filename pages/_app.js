ea0e70bcbe01be65dc1204825c3195fd6df0aa69
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-17 09:54:24 +0100

    added error boundary

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,47 @@
-import Layout from '../components/layout/layout'
-import Head from 'next/head'
-import '../styles/globals.css'
+
+import React from "react";
+import Layout from "../components/layout/layout";
+import Head from "next/head";
+import "../styles/globals.css";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    // Update state to indicate an error has occurred
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Log error information to an error reporting service
+    console.error("Error caught by ErrorBoundary:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Render fallback UI if an error has occurred
+      return <h1>Something went wrong.</h1>;
+    }
+
+    // Render children if no error
+    return this.props.children;
+  }
+}
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <ErrorBoundary>
+      <Layout>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </ErrorBoundary>
   );
 }
 
-export default MyApp
+export default MyApp;
